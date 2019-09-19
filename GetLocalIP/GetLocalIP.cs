a67ca2639ca0fe8d6a600dc96bead7a3fde28329
Elliot <elliot194@gmail.com>
2019-09-19 22:03:24 +0100

    V1
    Old C# Code to get local IP Addres.

---
+++ GetLocalIP/GetLocalIP.cs
@@ -0,0 +1,14 @@
+private string GetLocalIP()
+        {   // Handy function to get local ip 
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }        
